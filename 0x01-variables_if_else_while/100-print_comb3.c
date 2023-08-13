208db4a4a731911979602024d0d51458e5791e20
AyaaMahmoudd <ayamahmoudabdelfatah@gmail.com>
2023-08-13 13:50:39 -0700

    Inventing is a combination of brains and materials.

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -15,21 +15,21 @@ int main(void)
 	while (digit1 <= 9)
 	{
 		digit2 = 0;
-	while
-		(digit2 <= 9);
-	{
-		if (digit1 != digit2 && digit1 < digit2)
+		while
+			(digit2 <= 9);
 		{
+			if (digit1 != digit2 && digit1 < digit2)
+			{
 			putchar(digit1 + 48);
 			putchar(digit2 + 48);
 
-			if (digit1 + digit2 != 17)
-			{
-				putchar(',');
-				putchar(' ');
+				if (digit1 + digit2 != 17)
+				{
+					putchar(',');
+					putchar(' ');
+				}
 			}
-		}
-		digit2++;
+			digit2++;
 		}
 		digit1++;
 	}
